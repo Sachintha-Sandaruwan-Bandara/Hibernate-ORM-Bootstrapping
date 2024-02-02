9ed409387499e629ac97defad8967114b86ee9dc
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-02-02 16:22:48 +0530

    content update

---
+++ src/config/SessionFactoryConfig.java
@@ -0,0 +1,43 @@
+package config;
+/* 
+    @author Sachi_S_Bandara
+    @created 2/2/2024 - 3:54 PM 
+*/
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class SessionFactoryConfig {
+    private static SessionFactoryConfig factoryConfig;
+
+    private SessionFactoryConfig() {
+
+    }
+
+    public static SessionFactoryConfig getInstance() {
+//        if (null == factoryConfig) {
+//            factoryConfig = new SessionFactoryConfig();
+//        } else {
+//            return factoryConfig;
+//        }
+        return (null == factoryConfig)?factoryConfig=new SessionFactoryConfig():factoryConfig;
+
+
+    }
+    public Session getSession(){
+        //1.create service registry
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
+
+        //2.create a metadata object
+        Metadata metadata= new MetadataSources(serviceRegistry)/*.addAnnotatedClass()*/.getMetadataBuilder().build();
+
+        //3.create sessionFactory
+        SessionFactory sessionFactory = metadata.buildSessionFactory();
+
+        return sessionFactory.openSession();
+    }
+}
